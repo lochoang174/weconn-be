eab229775d68943f2bc8fc481a05257a684322e3
lochoang174 <lochoang611@gmail.com>
2025-07-18 12:33:46 +0000

    log connect db

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -12,18 +12,22 @@ export class DatabaseModule {
     const connections = Object.entries(databases).map(([name, configKey]) => {
       if (name === 'default') {
         return MongooseModule.forRootAsync({
-          useFactory: (configService: ConfigService) => ({
-            uri: configService.get<string>(configKey),
-          }),
+          useFactory: (configService: ConfigService) => {
+            const uri = configService.get<string>(configKey);
+            console.log(`Database connection [${name}] using key [${configKey}]:`, uri);
+            return { uri };
+          },
           inject: [ConfigService], 
         });
       }
       
       return MongooseModule.forRootAsync({
         connectionName: name,
-        useFactory: (configService: ConfigService) => ({
-          uri: configService.get<string>(configKey),
-        }),
+        useFactory: (configService: ConfigService) => {
+          const uri = configService.get<string>(configKey);
+          console.log(`Database connection [${name}] using key [${configKey}]:`, uri);
+          return { uri };
+        },
         inject: [ConfigService],
       });
     });
